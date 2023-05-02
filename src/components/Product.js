ebc86cdd1d0c5f2cf485696cbba9574b949e0c3e
Jhoggen <jacob.hoggen@medieinstitutet.se>
2023-05-02 10:46:43 +0200

    product update
    added prime logo

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,45 +10,55 @@ function Product({ id, price, description, category, image, title }) {
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
 
+  const [hasPrime] = useState(Math.random() < 0.5);
+
   return (
-    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
-      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
-        {category}
-      </p>
+      <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+        <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+          {category}
+        </p>
 
-      <div className="flex justify-center items-center">
-        <Image
-          placeholder="empty"
-          src={image}
-          height={200}
-          width={200}
-          className="object-contain"
-          alt="asd"
-        />
+        <div className="flex justify-center items-center">
+          <Image
+            placeholder="empty"
+            src={image}
+            height={200}
+            width={200}
+            className="object-contain"
+            alt="asd"
+          />
+        </div>
+
+        <h4 className="my-3">{title}</h4>
+
+        <div className="flex my-2">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <StarIcon className="h-5 text-yellow-500 " />
+            ))}
+        </div>
+
+        <p className="text-xs my-2 line-clamp-2">{description}</p>
+
+        <div className="mb-5">
+          <p>$ {price}</p>
+        </div>
+
+        {hasPrime && (
+          <div className="flex items-center space-x-2 -mt-5">
+            <Image
+              width={50}
+              height={50}
+              className="w-12"
+              src="https://links.papareact.com/fdw"
+              alt="Prime"
+            />
+            <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+          </div>
+        )}
+        <button className="button mt-auto">Add to basket</button>
       </div>
-
-      <h4 className="my-3">{title}</h4>
-
-      <div className="flex my-2">
-        {Array(rating)
-          .fill()
-          .map((_, i) => (
-            <StarIcon className="h-5 text-yellow-500 " />
-          ))}
-      </div>
-
-      <p className="text-xs my-2 line-clamp-2">{description}</p>
-
-      <div className="mb-5">
-        <p>$ {price}</p>
-      </div>
-
-      <div className="flex items-center space-x-2 -mt-5">
-        <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
-      </div>
-
-      <button className="button mt-auto">Add to basket</button>
-    </div>
   );
 }
 
